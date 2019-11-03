95336b8fb9851cae8fc905ffd9afd56d5c37ef64
samuel zaťovič <szatovic@gmail.com>
2019-11-03 16:09:42 +0100

    Hotový program

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_03._11
 {
@@ -6,7 +7,13 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - 1 - i])
+                    return false;
+                
+            }
+            return true;
         } 
     }
 }
--- Open-Lab-03.11/Program.cs
+++ Open-Lab-03.11/Program.cs
@@ -5,5 +5,6 @@ namespace Open_Lab_03._11
     class Program
     {
         static int Main(string[] args) => new AutoRun().Execute(args);
+
     }
 }
